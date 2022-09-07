e7f55e5cee9f35ba3a76e9959a61f083a2bf91c5
unknown <erskine.duenas@gmail.com>
2022-09-07 13:10:30 +0800

    added curriculum overview

---
+++ curriculumOverview.cpp
@@ -0,0 +1,21 @@
+Curriculum Overview:
+
+Beginning C++ Programming - From Beginner to Beyond (Dr. Frank Mitropoulos)
+
+-Structure of a C++ Program
+-Variables and Constants
+-Arrays and Vectors
+-Strings in C++
+-Expresisons, Statements and Operators 
+-Statements and operators 
+-Determining Control Flow 
+-Functions 
+-Pointers and References 
+-OOP - Classes and Objects 
+-Operator Overloading
+-Inheritance 
+-Polymorphism 
+-Smart Pointers 
+-The Standard Template Library (STL)
+-I/O Streams 
+-Exception Handling 
